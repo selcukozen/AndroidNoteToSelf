6addcb0e237512fc83adb194264fe421c571d73b
selcukozen <selcukozen82@gmail.com>
2021-09-01 22:24:00 +0300

    commit1

---
+++ app/src/main/java/com/example/notetoself/Note.java
@@ -0,0 +1,49 @@
+package com.example.notetoself;
+
+public class Note {
+	private String mTitle;
+	private String mDescription;
+	private boolean mIdea;
+	private boolean mTodo;
+	private boolean mImportant;
+
+	public String getmTitle() {
+		return mTitle;
+	}
+
+	public void setTitle(String mTitle) {
+		this.mTitle = mTitle;
+	}
+
+	public String getDescription() {
+		return mDescription;
+	}
+
+	public void setDescription(String mDescription) {
+		this.mDescription = mDescription;
+	}
+
+	public boolean isIdea() {
+		return mIdea;
+	}
+
+	public void setIdea(boolean mIdea) {
+		this.mIdea = mIdea;
+	}
+
+	public boolean isTodo() {
+		return mTodo;
+	}
+
+	public void setTodo(boolean mTodo) {
+		this.mTodo = mTodo;
+	}
+
+	public boolean isImportant() {
+		return mImportant;
+	}
+
+	public void setImportant(boolean mImportant) {
+		this.mImportant = mImportant;
+	}
+}
